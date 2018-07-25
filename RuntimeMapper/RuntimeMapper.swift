556bb36eca10ce8040991d39e82702886970cfac
ahn.thekan <ahn.thekan@navercorp.com>
2018-07-26 01:10:55 +0900

    create basic class RuntimeMapper

---
+++ RuntimeMapper/RuntimeMapper.swift
@@ -0,0 +1,14 @@
+//
+//  RuntimeMapper.swift
+//  RuntimeMapper
+//
+//  Created by 안덕환 on 26/07/2018.
+//  Copyright © 2018 thekan. All rights reserved.
+//
+
+import Foundation
+import Runtime
+
+public class RuntimeMapper {
+    
+}
