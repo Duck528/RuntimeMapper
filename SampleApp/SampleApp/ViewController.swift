a8c28be9ec6d9c46f4c8b7841849684adb161779
ahn.thekan <ahn.thekan@navercorp.com>
2018-07-30 02:05:25 +0900

    Sample App 테스트 코드 추가

--- SampleApp/SampleApp/ViewController.swift
+++ SampleApp/SampleApp/ViewController.swift
@@ -10,23 +10,41 @@ import UIKit
 import RuntimeMapper
 import Runtime
 
-class User {
-    var name = ""
-    var age = 0
+
+class Blog {
+    var id: Int = 0
+    var url: String = ""
+    var name: String = ""
 }
 
 class ViewController: UIViewController {
+    
+    let jsonString =
+    """
+    {
+        "blogs": [
+        {
+        "id": 111,
+        "url": "http://roadfiresoftware.com/blog/",
+        "name": "Roadfire Software Blog"
+        },
+        {
+        "id": 345,
+        "url": "https://developer.apple.com/swift/blog/",
+        "name": "Swift Developer Blog"
+        }]
+    }
+    """
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let runtimeMapper = RuntimeMapper()
-        try? runtimeMapper.read(from: "1234", initializer: User.init)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        if let blog = try? runtimeMapper.read(from: jsonString, initializer: Blog.init) {
+            print("id: \(blog.id)")
+            print("url \(blog.url)")
+            print("name: \(blog.name)")
+        }
     }
 }
 
