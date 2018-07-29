a172dae8b6f251f7125bf06c85517e17c30c8a0e
ahn.thekan <ahn.thekan@navercorp.com>
2018-07-30 01:14:26 +0900

    Add sampleApp for test

---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by 안덕환 on 30/07/2018.
+//  Copyright © 2018 thekan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
