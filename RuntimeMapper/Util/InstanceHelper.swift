892c977269ce63a944b15a193feb82d702a555b4
ahn.thekan <ahn.thekan@navercorp.com>
2018-08-17 16:53:06 +0900

    create InstanceHelper class

---
+++ RuntimeMapper/Util/InstanceHelper.swift
@@ -0,0 +1,48 @@
+//
+//  InstanceHelper.swift
+//  RuntimeMapper
+//
+//  Created by 안덕환 on 17/08/2018.
+//  Copyright © 2018 thekan. All rights reserved.
+//
+
+import Foundation
+import Runtime
+
+
+class InstanceHelper {
+    
+    enum Errors: Error {
+        case ParseInstanceError
+    }
+    
+    static func convertToDictionary<T>(from instance: T) throws -> [String: Any] {
+        guard let typeInfo = try? typeInfo(of: T.self) else {
+            throw Errors.ParseInstanceError
+        }
+        var dict: [String: Any] = [:]
+        for property in typeInfo.properties {
+            if let value = try? property.get(from: instance) {
+                dict[property.name] = value
+            }
+        }
+        return dict
+    }
+    
+    static func convertToDictionaries<T>(from instances: [T]) throws -> [[String: Any]] {
+        guard let typeInfo = try? typeInfo(of: T.self) else {
+            throw Errors.ParseInstanceError
+        }
+        var dicts: [[String: Any]] = [[:]]
+        for inst in instances {
+            var dict: [String: Any] = [:]
+            for property in typeInfo.properties {
+                if let value = try? property.get(from: inst) {
+                    dict[property.name] = value
+                }
+            }
+            dicts.append(dict)
+        }
+        return dicts
+    }
+}
